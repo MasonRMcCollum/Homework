f86940476fcb08495dece068b6809dd1d69a1d30
Mason McCollum <mccollummason34@gmail.com>
2022-05-03 13:36:13 -0500

    folderchanges

--- .vscode/Week2/week2homework.js
+++
@@ -1,9 +0,0 @@
-let numberOfToppings = 9;
-var pizzaPlace = ("The palace of pizza");
-
-console.log("Here at " + pizzaPlace + " ,we serve our pizza with " + numberOfToppings + " toppings, for our motto is")
-if (numberOfToppings < 10) {
-  console.log("Quality, not quantity.");
-} else if (numberOfToppings > 10){
-  console.log("A whole lot of pizza.")
-};
